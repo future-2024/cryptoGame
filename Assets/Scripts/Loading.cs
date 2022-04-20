4fe668a883d4ad7485fe031346078b2278579352
Roman Fedco <future55star@mail.ru>
2022-04-20 16:16:19 +0300

    fix

--- Assets/Scripts/Loading.cs
+++ Assets/Scripts/Loading.cs
@@ -33,11 +33,13 @@ public class Loading : MonoBehaviour
 
         //Print the time of when the function is first called.
         Debug.Log("Started Coroutine at timestamp : " + Time.time);
+
         //yield on a new YieldInstruction that waits for 2 seconds.
         yield return new WaitForSeconds(5);
 
         //After we have waited 2 seconds print the time again.
         Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+
         //And load the scene
         Application.LoadLevel(GlobalConstant.landList);
     }
